c007cdb2f3ce963d1c7c2c10e48ef64cff9eba8b
ArthurkaX <34007709+ArthurkaX@users.noreply.github.com>
2021-10-16 16:32:10 +0500

    Update Ex_12.py
    add user input check

--- Python basic P1/Ex_12.py
+++ Python basic P1/Ex_12.py
@@ -1,7 +1,11 @@
 # Write a Python program to print the calendar of a given month and year.
-
+import re
 import calendar
-input('Put here year and month in format: YYYY,MM >>>')
+while 1:
+    year_month = input('Put here year and month in format: YYYY,MM >>>')
+    if re.match('[0-9]{4},[0-9]{2}',year_month):
+        break
+    print('wrong input, pls retype...')
 cal = calendar.TextCalendar(firstweekday=0)
 
 for i in cal.itermonthdays(2021,10):
