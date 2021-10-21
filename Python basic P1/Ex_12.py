3c60c2aab63ee74a7072f61ef7c2bb244b07a538
ArthurkaX <34007709+ArthurkaX@users.noreply.github.com>
2021-10-21 13:34:18 +0500

    Update Ex_12.py
    solved

--- Python basic P1/Ex_12.py
+++ Python basic P1/Ex_12.py
@@ -6,21 +6,32 @@ while 1:
     if re.match('[0-9]{4},[0-9]{1,2}',year_month):
         year = int(year_month.split(',')[0])
         month = int(year_month.split(',')[1])
-        
-        break
+        if month > 12 or month <1:
+            print('month should be 1 - 12')
+        else:    
+            break
     print('wrong input, pls retype...')
 cal = calendar.TextCalendar(firstweekday=0)
-Mmatrix = cal.monthdayscalendar(year,month)
+daynames = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
+dates = cal.monthdayscalendar(year,month)
+
 print(cal.formatmonthname(year,month,10))
-print('Monday   Tuesday   Wednesday   Thursday   Friday   Satarday   Sunday', end = '')
-for x in Mmatrix:
+
+for x in daynames:
+    print(x, end='\t')
+
+for x in dates:
     print()
     for y in x:
-        if (y == 0):
-            print('        ', end = '')
-        else:
-            print(str(y) + '         ', end = '')    
+        if (y != 0):
+            print(y, end='')
+        print('\t', end='')
+        
 
 
-print()
-print(Mmatrix)
+#print(cal.itermonthdays20(year,month))
+#cal.pryear(year, w=2, l=1, c=6, m=3)
+
+y = int(input("Input the year : "))
+m = int(input("Input the month : "))
+print(calendar.month(y, m))
